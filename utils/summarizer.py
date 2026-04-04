78be2d7e762b74c830ada68bdb0c3d6650c56af3
Mustafa <razyrin@gmail.com>
2026-04-04 21:28:06 +0300

    CHANGE

--- utils/summarizer.py
+++ utils/summarizer.py
@@ -6,6 +6,14 @@ load_dotenv()
 API_KEY = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=API_KEY)
 
+def split_text(text, chunk_size=8000):
+    chunks = []
+    start = 0
+    while start < len(text):
+        chunks.append(text[start:start + chunk_size])
+        start += chunk_size
+    return chunks
+
 def generate_summary(text, language):
     try:
         prompt = f"""
